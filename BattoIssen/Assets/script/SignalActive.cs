2d9a565e0cf5eb1c312bed18d1285ab6368e4d28
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-12-10 15:54:21 +0900

    【add】Script SignalActiveを追加

---
+++ BattoIssen/Assets/script/SignalActive.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignalActive : MonoBehaviour 
+{
+   [SerializeField]
+  private  GameObject signal;
+   
+       
+    
+    void Start ()
+    {
+        //signalObjct=GameObject.Find("signal");
+       // Debug.Log(signalObjct);
+        
+
+       // gameObject.transform.Find("signal").gameObject.SetActive(true);
+        //signal = GameObject.Find("signal");
+        Debug.Log(signal);
+        signal.SetActive(false);
+        // IEnumerator Dela
+        Invoke("Delay", 1.5f);
+    }
+   /* IEnumerator Delay()
+    {
+        yield return null;
+        gameObject.transform.Find("signal").gameObject.SetActive(true);
+    }*/
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        
+    }
+    void Delay()
+    {
+       signal.SetActive(true);
+       
+
+    }    
+}
