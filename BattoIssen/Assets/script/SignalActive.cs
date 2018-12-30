b0ce13451c9dbdbbf5aa3e2d9dc11527bbfa307c
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-12-31 02:39:38 +0900

    【add】scriptスラッシュアクションを追加しました。

--- BattoIssen/Assets/script/SignalActive.cs
+++ BattoIssen/Assets/script/SignalActive.cs
@@ -3,42 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SignalActive : MonoBehaviour 
+public class SignalActive : MonoBehaviour
 {
-   [SerializeField]
-  private  GameObject signal;
-   
-       
-    
-    void Start ()
+    [SerializeField]
+     GameObject signal;
+    public bool Signalflag = false;
+
+
+    private void Awake()
+    {
+        signal.SetActive(true);
+    }
+
+    void Start()
     {
         //signalObjct=GameObject.Find("signal");
-       // Debug.Log(signalObjct);
-        
+        // Debug.Log(signalObjct);
 
-       // gameObject.transform.Find("signal").gameObject.SetActive(true);
+
+        // gameObject.transform.Find("signal").gameObject.SetActive(true);
         //signal = GameObject.Find("signal");
         Debug.Log(signal);
         signal.SetActive(false);
         // IEnumerator Dela
         Invoke("Delay", 1.5f);
     }
-   /* IEnumerator Delay()
-    {
-        yield return null;
-        gameObject.transform.Find("signal").gameObject.SetActive(true);
-    }*/
+    /* IEnumerator Delay()
+     {
+         yield return null;
+         gameObject.transform.Find("signal").gameObject.SetActive(true);
+     }*/
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
 
-        
+
     }
     void Delay()
     {
-       signal.SetActive(true);
-       
+        signal.SetActive(true);
+        Signalflag = true;
 
-    }    
+
+    }
 }
--- BattoIssen/Assets/script/SlashAction.cs
+++ BattoIssen/Assets/script/SlashAction.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class SlashAction : MonoBehaviour
 {
+    private float PushTime;
+    SignalActive signalgetFlag;
+    GameObject signal;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        signal = GameObject.Find("signal");
+        signalgetFlag = signal.GetComponent<SignalActive>();
+        Debug.Log(signal.name);
+        //Debug.LogError("notfind");
+        Debug.Log(signalgetFlag);
+        Debug.Log(signal);
+        PushTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+        Slash();
+    }
+
+    private void Slash()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("斬");
+            PushTime += Time.realtimeSinceStartup;
+            Debug.Log(PushTime);
+            if (signalgetFlag.Signalflag == true)
+            {
+                
+                Debug.Log("斬殺");
+            }
+            else
+            {
+                Debug.Log("no");
+            }
+            
+        }
+    }
 }
